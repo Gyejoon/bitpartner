96cf3a1111b4cca1f98c5f22e84bc0438c0ab8fd
dlrlwjd1127 <kj.lee@ilts.co.kr>
2018-06-06 13:10:40 +0900

    이기정 테스트

--- src/main/java/com/bitpartner/allspark/AllsparkApplication.java
+++ src/main/java/com/bitpartner/allspark/AllsparkApplication.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan
 public class AllsparkApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AllsparkApplication.class, args);
 	}
